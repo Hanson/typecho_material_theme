b358c1ad8e82c97b39308ca027e919e5fdf841cb
HanSon <h@hanc.cc>
2015-07-09 21:50:23 +0800

    Update archive.php

--- archive.php
+++ archive.php
@@ -13,7 +13,7 @@
                 <?php while($this->next()): ?>
                 <div class="panel panel-default">
                     <div class="panel-body">
-                        <h3 class="post-title"><a href="<?php $this->permalink() ?>" target="_blank"><?php $this->title() ?></a></h3>
+                        <h3 class="post-title"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h3>
                         <div class="post-meta">
                             <span>作者：<a href="<?php $this->author->permalink(); ?>"><?php $this->author(); ?></a> | </span>
                             <span>时间：<?php $this->date('F j, Y'); ?> | </span>
