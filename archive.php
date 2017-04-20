0c95077fd6edef254a5c849fdd4924d17c86431c
noisky <76698242@qq.com>
2017-04-21 00:30:05 +0800

    给部分搜索结果加上material效果

--- archive.php
+++ archive.php
@@ -9,7 +9,7 @@ $this->need('header.php');
         <div class="col-md-9">
             <div class="alert alert-success">您正在查看: <?php $this->archiveTitle(array(
                 'category'  =>  _t(' %s 分类下的文章'),
-                'search'    =>  _t('包含关键字 %s 的文章'),
+                'search'    =>  _t('包含关键字<span class="label label-success">%s</span>的文章'),
                 'tag'       =>  _t('标签 %s 下的文章'),
                 'author'    =>  _t('%s 发布的文章')
                 ), '', ''); ?></div>
@@ -20,18 +20,19 @@ $this->need('header.php');
                         <h3 class="post-title"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h3>
                         <div class="post-meta">
                             <span>作者：<a href="<?php $this->author->permalink(); ?>"><?php $this->author(); ?></a> | </span>
-                            <span>时间：<?php $this->date('F j, Y'); ?> | </span>
+                            <span>时间：<?php $this->date('Y-m-d'); ?> | </span>
                             <span>分类：<?php $this->category(','); ?> | </span>
                             <span>评论：<a href="<?php $this->permalink() ?>"><?php $this->commentsNum('%d 评论'); ?></a> </span>
                         </div>
-                        <div class="post-content"><?php $this->content('Continue Reading...'); ?></div>
+                        <div class="post-content"><?php $this->content('- 阅读剩余部分 -'); ?></div>
                     </div>
                 </div>
                 <?php endwhile; ?>
                 <?php else: ?>
-                    <article class="block">
-                        <h2 class="header"><?php _e('没有找到内容'); ?></h2>
-                    </article>
+                    <div class="alert alert-warning">
+                        <h4 class="header"><?php _e('找不到文章'); ?></h4>
+						<p>搜索结束，找不到<?php $this->archiveTitle(array('search'    =>  _t('包含关键字 %s 的文章'),), '', ''); ?></p>
+                    </div>
                 <?php endif; ?>
 
             <?php $this->pageNav('&laquo; 前一页', '后一页 &raquo;'); ?>
